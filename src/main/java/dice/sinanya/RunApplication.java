ade6a28ff455290ae0fb3ec88933ec89577ba526
张小舟 <zhangxiaozhou@rong360.com>
2019-06-04 19:47:00 +0800

    first commit

---
+++ src/main/java/dice/sinanya/RunApplication.java
@@ -0,0 +1,27 @@
+package dice.sinanya;
+
+import com.forte.qqrobot.component.forlemoc.LemocApp;
+import com.forte.qqrobot.component.forlemoc.LemocApplication;
+import com.forte.qqrobot.component.forlemoc.LinkConfiguration;
+import com.forte.qqrobot.sender.MsgSender;
+import com.forte.qqrobot.utils.CQCodeUtil;
+
+/**
+ * @author zhangxiaozhou
+ */
+public class RunApplication implements LemocApp {
+    public static void main(String[] args) {
+        new LemocApplication().run(new RunApplication());
+    }
+
+    @Override
+    public void before(LinkConfiguration configuration) {
+        configuration.setScannerPackage("dice.sinanya.listener");
+        configuration.setLinkIp("123.207.150.160");
+    }
+
+    @Override
+    public void after(CQCodeUtil cqCodeUtil, MsgSender sender) {
+
+    }
+}
