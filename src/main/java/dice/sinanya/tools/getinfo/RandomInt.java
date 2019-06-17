237e13413599002e8f045b59d91ac066443dc0d1
张小舟 <zhangxiaozhou@rong360.com>
2019-06-17 18:26:38 +0800

    整理马特赛特旋转演算法类中的warn

--- src/main/java/dice/sinanya/tools/getinfo/RandomInt.java
+++ src/main/java/dice/sinanya/tools/getinfo/RandomInt.java
@@ -1,17 +1,11 @@
 package dice.sinanya.tools.getinfo;
 
 import dice.sinanya.tools.makedata.MersenneTwister;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-import java.util.Random;
 
 /**
  * @author SitaNya
  */
 public class RandomInt {
-    private static Logger log = LogManager.getLogger(RandomInt.class.getName());
-
     public static int random(int lowest, int highest) {
         int result = 0;
         while (result == 0) {
