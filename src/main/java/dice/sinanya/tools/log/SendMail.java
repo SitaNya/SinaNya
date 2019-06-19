e35e899066315908c2a3bf1de4e5a1dac1bca848
张小舟 <zhangxiaozhou@rong360.com>
2019-06-19 10:35:14 +0800

    解决邮件内容乱码

--- src/main/java/dice/sinanya/tools/log/SendMail.java
+++ src/main/java/dice/sinanya/tools/log/SendMail.java
@@ -64,7 +64,7 @@ public class SendMail {
         // 设置收件人的邮箱
         mb.setSubject(toChinese(logName));
         // 设置邮件的主题
-        mb.setContent(toChinese("在群: " + groupId + " 中的log日志: " + logName + "\n其中docx为染色文件用word打开，无后缀为文本文件用txt打开，收到邮件烦请回复\n遇到问题请加群162279609进行反馈"));
+        mb.setContent("在群: " + groupId + " 中的log日志: " + logName + "\n其中docx为染色文件用word打开，无后缀为文本文件用txt打开，收到邮件烦请回复\n遇到问题请加群162279609进行反馈");
         // 设置邮件的正文
 
 //        这里取相对路径"bin/../saveLogs/${groupId}/${logName}"
