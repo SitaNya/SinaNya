c065d1f98f141c8a4818837f1d889bce260d2e80
sitanya <sitanya@qq.com>
2019-06-22 02:20:55 +0800

    群昵称bug修改

--- src/main/java/dice/sinanya/tools/getinfo/GetNickName.java
+++ src/main/java/dice/sinanya/tools/getinfo/GetNickName.java
@@ -30,11 +30,11 @@ public class GetNickName implements RootBean {
 
         switch (entityTypeMessages.getMsgGetTypes()) {
             case privateMsg:
-                return entityTypeMessages.getMsgSender().getPersonInfoByCode(entityTypeMessages.getFromQq()).getName();
+//                return entityTypeMessages.getMsgSender().getPersonInfoByCode(entityTypeMessages.getFromQq())getFromQq
             case groupMsg:
-                return entityTypeMessages.getMsgGroup().getUsername();
+//                return entityTypeMessages.getMsgGroup().getUsername();
             case discussMsg:
-                return entityTypeMessages.getMsgDisGroup().getNick();
+//                return entityTypeMessages.getMsgDisGroup().getNick();
             default:
                 entityTypeMessages.getMsgSender().SENDER.sendPrivateMsg("450609203", entityTypeMessages.toString());
                 return entityTypeMessages.toString();
@@ -48,15 +48,11 @@ public class GetNickName implements RootBean {
      * @return 昵称
      */
     public static String getGroupName(EntityTypeMessages entityTypeMessages) {
-        if (checkRoleChooseExistByFromQQ(entityTypeMessages)) {
-            return getRoleChooseByFromQQ(entityTypeMessages);
-        }
-
         switch (entityTypeMessages.getMsgGetTypes()) {
             case groupMsg:
-                return entityTypeMessages.getMsgGroup().getUsername();
+//                return entityTypeMessages.getMsgGroup().getUsername();
             case discussMsg:
-                return entityTypeMessages.getMsgDisGroup().getNick();
+//                return entityTypeMessages.getMsgDisGroup().getNick();
             default:
                 entityTypeMessages.getMsgSender().SENDER.sendPrivateMsg("450609203", entityTypeMessages.toString());
                 return entityTypeMessages.toString();
