6a8d58d944d868012aa9d711acd82f594b391e02
sitanya <sitanya@qq.com>
2019-06-25 00:00:38 +0800

    调整代码结构

--- bin/deamon.sh
+++ bin/deamon.sh
@@ -30,7 +30,7 @@ case ${startStop} in
     echo starting sinanya
     sinanyaScript='java -cp '"$bin"'/../lib/*:'"$bin"'/../conf/log4j2.xml:'"$bin"'/../conf/*:'"$Jar_Dir"' -Dlog4j.configurationFile='"$bin"'/../conf/log4j2.xml -Xms1024m -Xmx2048m dice.sinanya.RunApplication'
     echo ${sinanyaScript}
-    nohup ${sinanyaScript} >/dev/null 2>&1 &
+    nohup ${sinanyaScript} 2>&1 >> ../logs/all.log &
 
     echo $! > ${pid}
     ;;
@@ -99,7 +99,7 @@ case ${startStop} in
     echo starting sinanya
     sinanyaScript='java -cp '"$bin"'/../lib/*:'"$bin"'/../conf/*:'"$Jar_Dir"' -Dlog4j.configurationFile='"$bin"'/../conf/log4j2.xml -Xms1024m -Xmx2048m vdian.hadoop.sinanya.HDFS_sinanya'
     echo ${sinanyaScript}
-    nohup ${sinanyaScript} >/dev/null 2>&1 &
+    nohup ${sinanyaScript} 2>&1 >> ../logs/all.log &
 
     echo $! > ${pid}
     ;;
