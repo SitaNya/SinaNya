7ae029f64aa5a237f679f2c796df7b8f3829cd12
sitanya <sitanya@qq.com>
2019-06-29 02:56:54 +0800

    优化了分流逻辑

--- src/main/java/dice/sinanya/listener/Listener.java
+++ src/main/java/dice/sinanya/listener/Listener.java
@@ -65,7 +65,6 @@ public class Listener {
         EntityTypeMessages entityTypeMessages = new EntityTypeMessages(msgGetTypes, msgSender, msgGet, msgPrivate);
         if (entityTypeMessages.getMsgGet().getMsg().contains("bot")) {
             new Bot(entityTypeMessages).info();
-            setLogs(entityTypeMessages, msgGet);
         } else {
             new Flow(entityTypeMessages).toPrivate();
         }
