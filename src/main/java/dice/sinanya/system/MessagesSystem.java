eae7c34f1f17ccc270c1955a9a9f943a05d6da09
SitaNya <sitanya@qq.com>
2019-07-12 17:15:22 +0800

    修改新版本号，支持普罗米修斯监控系统

--- src/main/java/dice/sinanya/system/MessagesSystem.java
+++ src/main/java/dice/sinanya/system/MessagesSystem.java
@@ -14,7 +14,7 @@ public interface MessagesSystem {
     String NONE = "";
     String SPACE = " ";
 
-    String VERSIONS = "1.0.12.21";
+    String VERSIONS = "1.1.12.32";
 
     StringBuilder STR_BOT_VERSIONS = new StringBuilder()
             .append("Dice made in java\n")
