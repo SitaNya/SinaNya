b20abc61f147568ceba10b8a262abe15a5831d2e
SitaNya <sitanya@qq.com>
2019-07-23 15:53:52 +0800

    1.3.15.34
    增加每小时会自动清理不用群的功能
    增加心跳报告功能
    (测试阶段）

--- src/main/java/dice/sinanya/dice/system/Test.java
+++ src/main/java/dice/sinanya/dice/system/Test.java
@@ -8,7 +8,9 @@ import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 
+import static dice.sinanya.db.system.InsertBot.deleteBot;
 import static dice.sinanya.db.system.SelectBot.selectOffBotList;
+import static dice.sinanya.system.MessagesLoginInfo.ENTITY_LOGINQQ_INFO;
 import static dice.sinanya.tools.getinfo.GetNickName.getGroupName;
 
 /**
@@ -36,13 +38,26 @@ public class Test implements MakeNickToSender {
     private void autoClean(MsgSender msgSender) {
         ArrayList<String> offBotList = selectOffBotList();
         for (String offBotGroupId : offBotList) {
-            try {
+            if (msgSender.GETTER.getGroupInfo(offBotGroupId).getTypeId() == null) {
+                deleteBot(offBotGroupId);
+                continue;
+            }
+            long lastMsg = msgSender.GETTER.getGroupMemberInfo(offBotGroupId, String.valueOf(ENTITY_LOGINQQ_INFO.getLoginQQ())).getLastTime();
+            if (lastMsg > 21600) {
+                msgSender.SENDER.sendPrivateMsg("450609203", "已清理15日未使用，且已关闭本骰的群: " + makeGroupNickToSender(getGroupName(msgSender, offBotGroupId) + offBotGroupId));
+                deleteBot(offBotGroupId);
                 int type = msgSender.GETTER.getGroupInfo(offBotGroupId).getTypeId();
-                msgSender.SENDER.sendPrivateMsg("450609203", "type: " + type + "名称: " + makeGroupNickToSender(getGroupName(msgSender, offBotGroupId) + offBotGroupId));
-            } catch (NullPointerException e) {
-                log.error(e.getMessage(), e);
-                log.error(getGroupName(msgSender, offBotGroupId));
-                log.error(msgSender.GETTER.getGroupInfo(offBotGroupId).getOriginalData());
+                log.error("获取类型为: " + type);
+                if (type == 1) {
+                    msgSender.SENDER.sendPrivateMsg("450609203", "已清理15日未使用，且已关闭本骰的讨论组: " + makeGroupNickToSender(getGroupName(msgSender, offBotGroupId) + offBotGroupId));
+//                    msgSender.SENDER.sendDiscussMsg(offBotGroupId, "已在群: " + offBotGroupId + "中超过15日未响应且处于关闭状态，即将退群。");
+//                    msgSender.SETTER.setDiscussLeave(offBotGroupId);
+                } else {
+                    msgSender.SENDER.sendPrivateMsg("450609203", "已清理15日未使用，且已关闭本骰的群: " + makeGroupNickToSender(getGroupName(msgSender, offBotGroupId) + offBotGroupId));
+//                    msgSender.SENDER.sendGroupMsg(offBotGroupId, "已在群: " + offBotGroupId + "中超过15日未响应且处于关闭状态，即将退群。");
+//                    msgSender.SETTER.setGroupLeave(offBotGroupId);
+                }
+                return;
             }
         }
     }
