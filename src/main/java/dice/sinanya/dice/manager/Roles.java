a73487f0fdabfa35f940acbe5e0f204b205d3e88
sitanya <sitanya@qq.com>
2019-07-31 23:44:29 +0800

    1.3.15.37
    en允许数字，但数字格式不会录入进人物卡

--- src/main/java/dice/sinanya/dice/manager/Roles.java
+++ src/main/java/dice/sinanya/dice/manager/Roles.java
@@ -102,6 +102,8 @@ public class Roles implements Role {
         for (Map.Entry<EntityRoleTag, HashMap<String, Integer>> mapEntry : ROLE_INFO_CACHE.entrySet()) {
             if (checkRoleChooseExistByQQ(qqId) && !getRoleChooseByQQ(qqId).equals(mapEntry.getKey().getRole()) && mapEntry.getKey().getQq() == qqId) {
                 standbyRole.append(mapEntry.getKey().getRole()).append("\n");
+            } else if (mapEntry.getKey().getQq() == qqId) {
+                standbyRole.append(mapEntry.getKey().getRole()).append("\n");
             }
         }
         if (!stringBuilder.toString().contains("自定义")) {
